b6f057df29b48463b366251b219eac2edcdfa415
Jonathan Zacsh <jzacsh@gmail.com>
2011-07-19 22:51:51 -0400

    making sure `parseDir()` actually exists, first.

--- imagedex.js
+++ imagedex.js
@@ -5,6 +5,42 @@ var util = require('util');
 
 var index = {};
 
+var isDir = function (path) {
+  var directory = new Boolean(false);
+  fs.stat(path, function(err, stat) {
+    if (!err) {
+      console.log('isDir().. `stat` object:\n\t%s\n',
+        util.inspect(stat.isDirectory()));
+      directory = stat.isDirectory();
+    }
+  });
+  return directory;
+}
+
+var fileType = function (p) {
+  var dot = p.substring((p.length - 4), (p.length - 3));
+  if (dot === '.') {
+    return p.substring((p.length - 3))
+      .toLowerCase();
+  }
+  else {
+    return null;
+  }
+}
+
+var dirs = (function () {
+  if (process.argv.length > 2) {
+    return process.argv.slice(2);
+  }
+  else {
+    return [
+      './public/paper/',
+      './public/tablet/'
+    ];
+  }
+})();
+
+
 var parseDir = function(dir) {
   console.log('... parsing directory: %s\n', dir);
   fs.readdir(dir, function(err, files) {
@@ -23,7 +59,7 @@ var parseFiles = function(path, f) {
     var fullPath = path + f[file];
 
     if (isDir(fullPath)) {
-      console.log(); //@TODO: remove me!!    
+      console.log('...found directory, recursing:\n\t%s', fullPath); //@TODO: remove me!!    
       parseDir(fullPath);
     }
     else {
@@ -48,44 +84,8 @@ var parseFiles = function(path, f) {
   }
 }
 
-var isDir = function (path) {
-  fs.stat(path, function(err, stat) {
-    if (!err) {
-      console.log('isDir().. `stat` object:\n\t%s\n',
-        util.inspect(stat.isDirectory()));
-      return stat.isDirectory();
-    }
-  });
-}
-
-var fileType = function (p) {
-  var dot = p.substring((p.length - 4), (p.length - 3));
-  if (dot === '.') {
-    return p.substring((p.length - 3))
-      .toLowerCase();
-  }
-  else {
-    return null;
-  }
-}
-
 console.log('...starting'); //@TODO: remove me!!    
-
-var dirs = (function () {
-  if (process.argv.length > 2) {
-    return process.argv.slice(2);
-  }
-  else {
-    return [
-      './public/',
-      './public/paper/',
-      './public/tablet/',
-    ];
-  }
-})();
 
 for (var d in dirs) {
   parseDir(dirs[d]);
 }
-
-console.log('...done'); //@TODO: remove me!!    
