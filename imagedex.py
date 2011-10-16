468e838dc13a24976e07a0e0cd9b6f135dd69eda
Jonathan Zacsh <jzacsh@gmail.com>
2011-10-16 00:27:51 -0400

    proper flow for utility

--- imagedex.py
+++ imagedex.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python2.6
 
+import os
+import sys
 import simplejson as json
 
-#test data
-index = [ 'file1', 'file2' ]
+def usage():
+  usage = """
+  Usage: %s PATH
+    Generate JSON representation of the given directory, PATH.
+  """ % (sys.argv[0])
+  sys.stderr.write('%s\n' % usage)
 
-#finally output data as JSON
-print json.dumps(index)
+def main():
+  if len(sys.argv) != 2:
+    usage()
+    sys.exit(1)
+
+  #get an actual index of requested path
+  index = os.listdir(sys.argv[1]) #test data
+
+  #finally output data as JSON
+  print json.dumps(index)
+
+if __name__ == '__main__':
+  main()
