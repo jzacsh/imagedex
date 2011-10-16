d915331f7d3f8432ea4a8b8ae865fcb9712f675e
Jonathan Zacsh <jzacsh@gmail.com>
2011-10-16 13:37:49 -0400

    Start of a CLI wrapper of around imagedex/ module.

---
+++ imagedex/imagedex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+"""
+  Imagedex
+
+  Command-line utility wrapping imagedex methods.
+"""
+
+import ../imagedex
+
+
+if __name__ == '__main__':
+  main()
+
+# vim: et:ts=4:sw=4:sts=4
