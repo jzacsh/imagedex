066c32f7c5d66eea7c8fc1529434b92ba3278e5e
Jonathan Zacsh <jzacsh@gmail.com>
2011-10-17 22:56:26 -0400

    Learning the awesome that is Jinja templating

--- examples/slides/__init__.py
+++ examples/slides/__init__.py
@@ -3,9 +3,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/art/')
 @app.route('/art/<slidefile>')
-def slideshow(slidefile):
-  return render_template('slides.html', slide=slidefile)
+def slideshow(slidefile=None):
+  return render_template('slides.html', data = {
+      'title': "Jonathan Zacsh's Drawings",
+      'slide': slidefile,
+      })
 
 if __name__ == '__main__':
   app.run(debug = True)
